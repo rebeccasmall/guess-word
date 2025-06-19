d41d8cf984f8f70290d82cc9bd771e5b6125f1c5
rebeccasmall <rebecca.small18@hotmail.com>
2025-06-19 12:23:36 -0400

    Finished grabbing query selectors for all elements

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const guessedLetters = document.querySelector(".guessed-letters");
+// Unordered list where the guessed letters will appear
+const guessButton = document.querySelector(".guess");
+// Button to submit guess
+const input = document.querySelector(".letter");
+// Text input where player will guess letter 
+const wordInProgress = document.querySelector(".word-in-progress");
+// Empty paragraph where the word in progress will appear 
+const remaining = document.querySelector(".remaining");
+// Paragraph where the remaining guesses will display
+const span = document.querySelector("span");
+// Span element where the remaining guesses will appear
+const message = document.querySelector(".message");
+// Empty paragraph where message will appear when player gueses a letter
+const playAgainButton = document.querySelector(".play-again");
