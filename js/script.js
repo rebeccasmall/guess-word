617351dc141e44b8eff5de640eb06a52716f26e4
rebeccasmall <rebecca.small18@hotmail.com>
2025-06-26 15:19:23 -0400

    Added async function

--- js/script.js
+++ js/script.js
@@ -18,6 +18,14 @@ const word = "magnolia";
 const guessedLetters = [];
 let remainingGuesses = 8;
 
+const getWord = async function () {
+    const res = await fetch ("https://gist.githubusercontent.com/skillcrush-curriculum/7061f1d4d3d5bfe47efbfbcfe42bf57e/raw/5ffc447694486e7dea686f34a6c085ae371b43fe/words.txt");
+    const data = await res.text();
+    console.log(data);
+}
+
+getWord();
+
 const updateWordProgress = function () {
     let placeholders = [];
     for (let i = 0; i < word.length; i++) {
